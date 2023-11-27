0b17c0f29722d6645f16f44f46a4016016e693ec
Haru <haru-o94>
2023-11-27 17:41:29 +0200

    edit file

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,27 @@
 #include "lists.h"
 
 /**
- * check_cycle - check from the linked
- *
- * Return: 1 if have cycle
+ * check_cycle - check of linked
+ * @list: pointer
+ * Return: 1 have a cycle
  */
 int check_cycle(listint_t *list)
 {
-    if (!list)
-        return 0;
+	listint_t *ptr1 = NULL;
+	listint_t *ptr2 = NULL;
 
-    listint_t *sl = list;
-    listint_t *fa = list;
+	if (list == NULL)
+		return (0);
+	ptr1 = list;
+	ptr2 = list;
 
-    for (; sl && fa && fa->next; sl = sl->next, fa = fa->next->next)
-    {
-        if (sl == fa)
-            return 1;
-    }
+	while (ptr1 && ptr2 && ptr2->next)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
 
-    return 0;
+		if (ptr1 == ptr2)
+			return (1);
+	}
+	return (0);
 }
